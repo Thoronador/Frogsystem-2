49062b25bf8fe8377149ae4384736a6889c0acd7
Thoronador <thoronador@users.sourceforge.net>
2012-08-11 02:27:57 +0200

    add missing authorship note in comments

--- www/admin/admin_persistentedit.php
+++ www/admin/admin_persistentedit.php
@@ -2,6 +2,7 @@
 /*
     Frogsystem Persistent Worlds Scripts
     Copyright (C) 2005-2007  Stefan Bollmann
+    Copyright (C) 2012  Thoronador (adjustments for alix5)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
